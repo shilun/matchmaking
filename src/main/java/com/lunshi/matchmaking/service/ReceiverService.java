d1cfd93c563c91544af140e4425dac01d959954a
lunsh <martin.shi@nidone.com.cn>
2018-08-05 07:35:05 +0800

    初始化

--- src/main/java/com/lunshi/matchmaking/service/ReceiverService.java
+++ src/main/java/com/lunshi/matchmaking/service/ReceiverService.java
@@ -8,43 +8,22 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
 /**
  * 撮合接收通知服务
  */
-public class ReceiverService {
-    List<ItemInfo> sells = new ArrayList<ItemInfo>();
-
-    List<ItemInfo> buys = new ArrayList<ItemInfo>();
+public interface ReceiverService {
 
     /**
      * 新增交易
      *
      * @param item
      */
-    public void addItem(ItemInfo item) {
-        if (item.getType() == ItemTypeEnum.SELL) {
-            sells.add(item);
-        }
-        if (item.getType() == ItemTypeEnum.BUY) {
-            buys.add(item);
-        }
-
-    }
+    public void addItem(ItemInfo item);
 
     /**
-     * 执行撮合
+     * 对撮合接收到的数据加入撮合线程
+     *
      * @param sellList
      */
-    public synchronized void doExecute(List<ItemInfo> sellList, List<ItemInfo> buyList) {
-        synchronized (sells) {
-            sellList.addAll(sells);
-            sells.clear();
-            synchronized (buys) {
-                buyList.addAll(buys);
-                buys.clear();
-            }
-
-        }
-    }
+    public void doExecute(List<ItemInfo> sellList, List<ItemInfo> buyList);
 }
